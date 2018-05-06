51fb1058bf3be3d9442063d6d60c9668ae7c0adc
osito_wang <wangren0518@outlook.com>
2018-05-06 11:09:15 -0400

    Read into binary file and modify it

--- stringSplit.py
+++ stringSplit.py
@@ -1,5 +1,7 @@
 import re
 import os ,stat
+import struct
+import array
 # Split the document with multiple seperator
 # s = "ab:cd|efg\t|hi,jkkl"
 #
@@ -14,14 +16,63 @@ import os ,stat
 # print(split_multiple(s, ":,|\t"))
 
 # using regular expression
-# print(re.split("[:,|\t]+", s))
+# # print(re.split("[:,|\t]+", s))
+#
+# print(os.listdir('.'))
+#
+# name_list = [name for name in os.listdir('.') if name.endswith((".py",".txt"))]
+#
+# print(name_list)
+#
+# print(oct(os.stat('List_Dict.py').st_mode))
+#
+# os.chmod("List_Dict.py",os.stat('List_Dict.py').st_mode| stat.S_IXUSER)
+#
+#
+# # Replace the original data with new data format
+#
+# log = open("mylog.log").read()
+#
+# re.sub("(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})", r"\g<month>/\g<day>/\g<year>", log)
 
-print(os.listdir('.'))
 
-name_list = [name for name in os.listdir('.') if name.endswith((".py",".txt"))]
+# Read bynary files
 
-print(name_list)
+f = open('demo.wav', 'rb')
+info = f.read(44)
 
-print(oct(os.stat('List_Dict.py').st_mode))
+print(f.read(44))
+print(struct.unpack('h', info[34:36]))
 
-os.chmod("List_Dict.py",os.stat('List_Dict.py').st_mode| stat.S_IXUSER)
+f.seek(0, 2)
+print(f.tell())
+
+n = int((f.tell()-44)/2)
+
+print(n)
+
+buf = array.array("h", [])
+f.seek(44)
+buf.fromfile(f, n)
+
+print(len(buf))
+
+for x in range(0, n):
+    buf[x] = int(buf[x]/8)
+    print(buf[x])
+
+f2 = open("demo2.wav", "wb")
+
+f2.write(info)
+
+buf.tofile(f2)
+
+f2.close()
+
+
+
+
+
+
+
+
